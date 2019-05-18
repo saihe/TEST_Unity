100f33ee41050078393811670915340b642400ec
Kyohei Saito <saihe.kyon@gmail.com>
2019-05-18 22:24:54 +0900

    バーチャルジョイスティック実装。ボールを動かせるように。

--- SandBox/Assets/Scripts/Sphere.cs
+++ SandBox/Assets/Scripts/Sphere.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Sphere : MonoBehaviour
 {
-    public float jumpPower;
+    public float speed;
+    public FloatingJoystick floatingJoystick;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
-        }
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
     }
 }
